3476a0319eeffcf46dfb327a973a867d271515b4
ragilhadi <ragilprasetyo310@gmail.com>
2020-03-22 14:47:38 +0700

    Updates

---
+++ precache-manifest.10231f146663b2edfc625ffcbe7f0b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b48c15bb0c8a9830bd938f8fe96039",
+    "url": "/monster-card/index.html"
+  },
+  {
+    "revision": "3fff68d67a1c9b93510e",
+    "url": "/monster-card/static/css/main.067e496a.chunk.css"
+  },
+  {
+    "revision": "dadd5e5bc1621b3aa903",
+    "url": "/monster-card/static/js/2.d6620123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-card/static/js/2.d6620123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fff68d67a1c9b93510e",
+    "url": "/monster-card/static/js/main.bfb84a55.chunk.js"
+  },
+  {
+    "revision": "0b2666e90f29cdf1e08f",
+    "url": "/monster-card/static/js/runtime-main.6f55a641.js"
+  }
+]);
